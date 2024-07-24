f7c0a6f1e28c5811f7417e43a18f69e44964141f
Juliana_França <juliana_souza_franca@hotmail.com>
2024-07-24 10:31:48 -0300

    Deploy Severest

--- cypress.config.js
+++ cypress.config.js
@@ -3,20 +3,22 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://front.serverest.dev/login',
+    
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
 
+
+
+    screenshotOnRunFailure: true,
+    screenshotsFolder: 'cypress/screenshots',
+    video: true,
+    videoCompression: false,
+    videosFolder: 'cypress/videos',
+    defaultCommandTimeout: 10000,
+    fixturesFolder: 'cypress/fixtures'
   },
 
-  screenshotOnRunFailure: true,
-  screenshotsFolder: 'cypress/screenshots',
-  video: true,
-  videoCompression: false,
-  videosFolder: 'cypress/videos',
-  defaultCommandTimeout: 10000,
-  integrationFolder: 'cypress/integration',
-  fixturesFolder: 'cypress/fixtures'
    
 });
