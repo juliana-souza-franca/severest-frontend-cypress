c24ae13562e919d8813eeb6fbbe4d7f95482d3d5
Juliana_França <juliana_souza_franca@hotmail.com>
2024-07-24 11:31:11 -0300

    Deploy

--- cypress.config.js
+++ cypress.config.js
@@ -3,18 +3,23 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://front.serverest.dev/login',
-    baseUrl: 'http://localhost:3000',
-    integrationFolder: 'cypress/integration',
-    fixturesFolder: 'cypress/fixtures',
-    videosFolder: 'cypress/videos',
-    screenshotsFolder: 'cypress/screenshots',
+    
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
 
 
+
+  screenshotOnRunFailure: true,
+  screenshotsFolder: 'cypress/screenshots',
+  video: true,
+  videoCompression: false,
+  videosFolder: 'cypress/videos',
+  defaultCommandTimeout: 10000,
+  fixturesFolder: 'cypress/fixtures'
   },
 
+ 
    
 });
